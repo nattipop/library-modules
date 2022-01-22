32de629463cc054f5107d2dba054152cc4982876
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2022-01-21 21:41:28 -0600

    solved problem

--- main.js
+++ main.js
@@ -1,1 +1,53 @@
 // write your code here to make the tests pass
+var Library = function() {
+  var books = [];
+
+  var checkOutBook = function(book) {
+    if(books.includes(book)) {
+      book.setAttribute('checkedOut', true)
+    }
+  };
+
+  var returnBook = function(book) {
+    if(books.includes(book)) {
+      book.setAttribute('checkedOut', false);
+    }
+  };
+
+  var addBook = function(book) {
+    books.push(book);
+  };
+
+  return {
+    checkOutBook: checkOutBook,
+    returnBook: returnBook,
+    addBook: addBook,
+  }
+};
+
+var library = Library();
+
+var Book = function(title, author) {
+  var attributes = {
+    title: title,
+    author: author,
+    checkedOut: false,
+  };
+
+  var getAttribute = function(property) {
+    if (attributes.hasOwnProperty(property)) {
+      return attributes[property];
+    }
+  };
+
+  var setAttribute = function(property, value) {
+    if (attributes.hasOwnProperty(property)) {
+      attributes[property] = value;
+    }
+  };
+
+  return {
+    getAttribute: getAttribute,
+    setAttribute: setAttribute,
+  };
+};
